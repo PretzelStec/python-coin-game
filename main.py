a9fa9440972c5d340594a528c34fa112cdaf35a9
unknown <jakestec99@gmail.com>
2021-03-19 17:24:32 -0500

    added more stuffs

--- main.py
+++ main.py
@@ -1,43 +1,66 @@
 import arcade
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
-PLAYER_SCALE = 0.05
-MOVEMENT_SPEED = 8
+#game setup
+SCREEN_WIDTH = 750
+SCREEN_HEIGHT = 750
+MOVEMENT_SPEED = 5
+
+# empty level template
+# [
+#     ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
+#     ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+#     ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+#     ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+#     ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+#     ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+#     ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+#     ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+#     ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+#     ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
+# ]
+
+
+#levels setup
 current_level = 0
-LEVELS_MAX_COINS = [3, 4, 4]
+LEVEL_COUNT = 3
+LEVELS_MAX_COINS = [4, 1, 2]
+LEVELS_MAX_SECONDS = [100, 100, 100]
 LEVELS = [
         [
-            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
-            ['w', ' ', ' ', ' ', ' ', ' ', 'p', 'w'],
-            ['w', 'c', 'w', 'w', ' ', 'w', 'w', 'w'],
-            ['w', 'w', 'w', 'w', ' ', ' ', ' ', 'w'],
-            ['w', 'c', 'w', 'w', ' ', 'w', 'w', 'w'],
-            ['w', ' ', 'w', ' ', ' ', ' ', ' ', 'w'],
-            ['w', ' ', ' ', ' ', 'w', ' ', 'c', 'w'],
-            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
+            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
+            ['w', 'c', ' ', ' ', ' ', ' ', ' ', 'w', 'c', 'w'],
+            ['w', 'w', 'w', 'w', 'w', 'w', ' ', 'w', ' ', 'w'],
+            ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+            ['w', ' ', 'w', 'w', 'w', 'w', ' ', ' ', 'w', 'w'],
+            ['w', ' ', ' ', 'w', 'c', 'w', ' ', 'w', 'w', 'w'],
+            ['w', 'w', ' ', ' ', ' ', 'w', ' ', ' ', ' ', 'w'],
+            ['w', 'c', 'w', 'w', 'w', 'w', ' ', 'w', 'w', 'w'],
+            ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'p', 'w'],
+            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
         ],
-
         [
-            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
-            ['w', 'c', ' ', ' ', ' ', ' ', ' ', 'w'],
-            ['w', 'c', 'w', 'w', ' ', 'w', 'w', 'w'],
-            ['w', 'w', 'w', 'w', ' ', ' ', 'p', 'w'],
-            ['w', 'c', 'w', 'w', ' ', 'w', 'w', 'w'],
-            ['w', ' ', 'w', ' ', ' ', ' ', ' ', 'w'],
-            ['w', ' ', ' ', ' ', 'w', ' ', 'c', 'w'],
-            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
+            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
+            ['w', ' ', ' ', ' ', 'w', 'w', ' ', 'w', ' ', 'w'],
+            ['w', ' ', 'w', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+            ['w', ' ', 'w', ' ', 'w', 'w', 'w', 'w', ' ', 'w'],
+            ['w', ' ', 'w', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+            ['w', ' ', 'w', ' ', 'w', 'w', ' ', 'w', ' ', 'w'],
+            ['w', ' ', 'w', 'p', 'w', ' ', 'w', ' ', ' ', 'w'],
+            ['w', ' ', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
+            ['w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'c', 'w'],
+            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
         ],
-
         [
-            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
-            ['w', 'c', ' ', ' ', ' ', ' ', ' ', 'w'],
-            ['w', 'c', 'w', 'w', ' ', 'w', 'w', 'w'],
-            ['w', 'w', 'w', 'w', ' ', ' ', ' ', 'w'],
-            ['w', 'c', 'w', 'w', ' ', 'w', 'w', 'w'],
-            ['w', ' ', 'w', ' ', 'p', ' ', ' ', 'w'],
-            ['w', ' ', ' ', ' ', 'w', ' ', 'c', 'w'],
-            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
+            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
+            ['w', ' ', ' ', ' ', ' ', ' ', 'w', 'c', ' ', 'w'],
+            ['w', ' ', 'w', 'w', 'w', ' ', 'w', 'w', ' ', 'w'],
+            ['w', ' ', 'w', 'w', ' ', 'p', ' ', ' ', ' ', 'w'],
+            ['w', ' ', ' ', ' ', 'w', ' ', 'w', 'w', ' ', 'w'],
+            ['w', ' ', 'w', ' ', 'w', ' ', ' ', ' ', 'w', 'w'],
+            ['w', ' ', ' ', ' ', 'w', ' ', 'w', ' ', ' ', 'w'],
+            ['w', 'w', 'w', 'w', ' ', 'w', ' ', 'w', ' ', 'w'],
+            ['w', 'c', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w'],
+            ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
         ]
 ]
 
@@ -59,12 +82,12 @@ class Level(arcade.View):
         self.player_sight_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
-
+        self.total_time = 0.0
         
         self.score = 0
 
         #build wall
-        self.build_level_from_matrix(self.levelMatrix)
+        self.build_level_from_matrix(self.levelMatrix, 0.75)
         
 
     def on_draw(self):
@@ -76,47 +99,53 @@ class Level(arcade.View):
         self.coin_list.draw()
         self.player_sight_list.draw()
 
-        output = f"Score: {self.score}"
+        output = f"Score: {self.score} / {self.max_score}"
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+        timeleft = LEVELS_MAX_SECONDS[current_level]-int(self.total_time) % 60
+        output = f"Time remaining: {timeleft}"
+        if timeleft <= 5:
+            arcade.draw_text(output, 10, 35, arcade.color.RED, 14)
+        else:
+            arcade.draw_text(output, 10, 35, arcade.color.WHITE, 14)
 
-    def add_wall(self, x, y):
-        wall = arcade.Sprite('./wallblock.png', 1)
+    def add_wall(self, x, y, scale : float = 1):
+        wall = arcade.Sprite('./wallblock.png', 1 * scale)
         wall.center_x=x
         wall.center_y=y
         self.wall_list.append(wall)
 
-    def add_player(self, x, y):
-        self.playerSprite = arcade.Sprite('./smile.png', 0.05)
+    def add_player(self, x, y, scale : float = 1):
+        self.playerSprite = arcade.Sprite('./smile.png', 0.05 * scale)
         self.playerSprite.center_x = x
         self.playerSprite.center_y = y
         self.player_list.append(self.playerSprite)
 
-        self.player_sight = arcade.Sprite('./playerSight.png', 1)
+        self.player_sight = arcade.Sprite('./playerSight.png', 1 * scale)
         self.player_sight.center_x = x
         self.player_sight.center_y = y
         self.player_sight_list.append(self.player_sight)
 
-    def add_coin(self, x, y):
-        coin = arcade.Sprite('./coin.png', 0.5)
+    def add_coin(self, x, y, scale : float = 1):
+        coin = arcade.Sprite('./coin.png', 0.5 * scale)
         coin.center_x = x
         coin.center_y = y
         self.coin_list.append(coin)
 
-    def build_level_from_matrix(self, matrix):
-        pos_y = 50
-        pos_x = 50
+    def build_level_from_matrix(self, matrix, scale : float = 1):
+        pos_y = 50 * scale
+        pos_x = 50 * scale
 
         for x in range(len(matrix)):
             for y in range(len(matrix[x])):
                 if(matrix[len(matrix)-x-1][y] == 'w'):
-                    self.add_wall(pos_x, pos_y)
+                    self.add_wall(pos_x, pos_y, scale)
                 if(matrix[len(matrix)-x-1][y] == 'p'):
-                    self.add_player(pos_x, pos_y)
+                    self.add_player(pos_x, pos_y, scale)
                 if(matrix[len(matrix)-x-1][y] == 'c'):
-                    self.add_coin(pos_x, pos_y)
-                pos_x += 100
-            pos_x = 50
-            pos_y += 100
+                    self.add_coin(pos_x, pos_y, scale)
+                pos_x += 100 * scale
+            pos_x = 50 * scale
+            pos_y += 100 * scale
 
         self.physics_engine = arcade.PhysicsEngineSimple(self.playerSprite, self.wall_list)
         
@@ -147,6 +176,13 @@ class Level(arcade.View):
 
     def update(self, delta_time):
         """ All the logic to move, and the game logic goes here. """
+        global current_level 
+        self.total_time += delta_time
+
+        if self.total_time >= LEVELS_MAX_SECONDS[current_level]:
+            view = Failed()
+            self.window.show_view(view)
+
         self.physics_engine.update()
         
         self.player_sight.center_x = self.playerSprite.center_x
@@ -164,29 +200,54 @@ class Level(arcade.View):
             self.score += 1
 
         if self.score == self.max_score:
-            global current_level 
-            current_level += 1
-            view = Complete(True)
-            self.window.show_view(view)
+            if current_level < LEVEL_COUNT-1:
+                current_level += 1
+                view = Complete(self.total_time)
+                self.window.show_view(view)
+            else:
+                current_level = 0
+                view = Beat()
+                self.window.show_view(view)
+
 
 
 class Complete(arcade.View):
     """ Main application class. """
 
-    def __init__(self, success: bool = False):
+    def __init__(self, time : int = 0):
         super().__init__()
-        if success:
-            arcade.set_background_color(arcade.color.DARK_GREEN)
-            self.message = 'Complete'
-        else:
-            arcade.set_background_color(arcade.color.RED)
-            self.message = 'Failed'
+        arcade.set_background_color(arcade.color.DARK_GREEN)
+        self.message = 'Complete'
+        self.time = int(time)
 
     def on_draw(self):
         """ Draw this view """
         arcade.start_render()
         status = self.message
         arcade.draw_text(status, 20, SCREEN_HEIGHT//2, arcade.color.WHITE, 40)
+        arcade.draw_text(f'Time : {self.time} seconds', 20, SCREEN_HEIGHT//2-45, arcade.color.WHITE, 15)
+        arcade.draw_text('Click anywhere to start next level.', 20, SCREEN_HEIGHT//2-90, arcade.color.WHITE, 15)
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        """ If the user presses the mouse button, re-start the game. """
+        game_view = Level(LEVELS[current_level], LEVELS_MAX_COINS[current_level])
+        game_view.setup()
+        self.window.show_view(game_view)
+
+class Failed(arcade.View):
+    """ Main application class. """
+
+    def __init__(self):
+        super().__init__()
+        arcade.set_background_color(arcade.color.DARK_RED)
+        self.message = 'Failed'
+
+    def on_draw(self):
+        """ Draw this view """
+        arcade.start_render()
+        status = self.message
+        arcade.draw_text(status, 20, SCREEN_HEIGHT//2, arcade.color.WHITE, 40)
+        arcade.draw_text('Click anywhere to restart the level.', 20, SCREEN_HEIGHT//2-45, arcade.color.WHITE, 15)
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         """ If the user presses the mouse button, re-start the game. """
@@ -194,6 +255,26 @@ class Complete(arcade.View):
         game_view.setup()
         self.window.show_view(game_view)
         
+class Beat(arcade.View):
+    """ Main application class. """
+
+    def __init__(self):
+        super().__init__()
+        arcade.set_background_color(arcade.color.BLEU_DE_FRANCE)
+        self.message = 'You Beat the Game!!!!'
+
+    def on_draw(self):
+        """ Draw this view """
+        arcade.start_render()
+        status = self.message
+        arcade.draw_text(status, 20, SCREEN_HEIGHT//2, arcade.color.WHITE, 40)
+        arcade.draw_text('Click anywhere to restart the game.', 20, SCREEN_HEIGHT//2-45, arcade.color.WHITE, 15)
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        """ If the user presses the mouse button, re-start the game. """
+        game_view = Level(LEVELS[current_level], LEVELS_MAX_COINS[current_level])
+        game_view.setup()
+        self.window.show_view(game_view)
 
 
 def main():
